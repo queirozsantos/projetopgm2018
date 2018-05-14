f93bd266efbbe049f06e57cf961e15824d9392d8
Queiroz Santos <queiroz.ted@gmail.com>
2018-05-14 20:00:21 -0300

    implementando as classes basicas Photo e Place

---
+++ app/src/main/java/br/com/queirozted/projetopgm/basics/Photo.java
@@ -0,0 +1,43 @@
+package br.com.queirozted.projetopgm.basics;
+
+import java.io.Serializable;
+
+
+public class Photo implements Serializable {
+    String[]  html_attributions;
+    Long height;
+    Long width;
+    String photo_reference;
+
+    public String[] getHtml_attributions() {
+        return html_attributions;
+    }
+
+    public void setHtml_attributions(String[] html_attributions) {
+        this.html_attributions = html_attributions;
+    }
+
+    public Long getHeight() {
+        return height;
+    }
+
+    public void setHeight(Long height) {
+        this.height = height;
+    }
+
+    public Long getWidth() {
+        return width;
+    }
+
+    public void setWidth(Long width) {
+        this.width = width;
+    }
+
+    public String getPhoto_reference() {
+        return photo_reference;
+    }
+
+    public void setPhoto_reference(String photo_reference) {
+        this.photo_reference = photo_reference;
+    }
+}
---
+++ app/src/main/java/br/com/queirozted/projetopgm/basics/Place.java
@@ -0,0 +1,115 @@
+package br.com.queirozted.projetopgm.basics;
+
+import java.io.Serializable;
+
+
+
+public class Place implements Serializable {
+    private String name;
+    private String[] type;
+    private String formatted_address;
+    private Geometry geometry;
+    private Float rating;
+    private String icon;
+    private String reference;
+    private String id;
+    private Integer price_level;
+    private String  place_id;
+    private Photo photo;
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    public Place() {
+    }
+
+    public Place(String name, float rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getType() {
+        return type;
+    }
+
+    public void setType(String[] type) {
+        this.type = type;
+    }
+
+    public String getFormatted_address() {
+        return formatted_address;
+    }
+
+    public void setFormatted_address(String formatted_address) {
+        this.formatted_address = formatted_address;
+    }
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPrice_level() {
+        return price_level;
+    }
+
+    public void setPrice_level(Integer price_level) {
+        this.price_level = price_level;
+    }
+
+    public String getPlace_id() {
+        return place_id;
+    }
+
+    public void setPlace_id(String place_id) {
+        this.place_id = place_id;
+    }
+}
